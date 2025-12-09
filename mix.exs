b71b964e380aa336f71b103c729b5f0029857350
Melle Lieuwes <m.lieuwes@eyra.co>
2025-12-09 11:22:30 +0100

    Bump version to 0.1.4

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule LiveNest.MixProject do
   def project do
     [
       app: :live_nest,
-      version: "0.1.3",
+      version: "0.1.4",
       elixir: "~> 1.15",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
